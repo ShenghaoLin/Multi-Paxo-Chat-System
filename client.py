b19ff325ea4f8fb6a8a45caeba24818c67422a9c
ShenghaoLin <sl4549@nyu.edu>
2018-10-20 16:13:04 -0400

    hopefully work this time

--- client.py
+++ client.py
@@ -1,9 +1,20 @@
 import socket               # Import socket module
 import select
+from requests import get
+
+
+
+
 s = socket.socket()         # Create a socket object
 host = socket.gethostbyname(socket.gethostname()) # Get local machine name
 
+
+host = get('https://api.ipify.org').text
+
+
+
 print(host)
+
 
 port = 12345                # Reserve a port for your service.
 
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 import socket               # Import socket module
+from requests import get
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostbyname(socket.gethostname()) # Get local machine name
-
+host = '0.0.0.0'
 print(host)
 port = 12345                # Reserve a port for your service.
+
+
 s.bind((host, port))        # Bind to the port
 
 s.listen(5)                 # Now wait for client connection.
