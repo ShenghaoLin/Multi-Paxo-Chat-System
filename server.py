2b0877b1ea9da4eda5f42ceb8fed30d5cde7d00d
pearlswan <yangmingk@gmail.com>
2018-10-21 18:26:19 -0400

    update server reply

--- server.py
+++ server.py
@@ -19,8 +19,9 @@ def service_connection(key, mask):
     data = key.data
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(1024)  # Should be ready to read
+        reply = 'Thank you received.'
         if recv_data:
-            data.outb += recv_data
+            data.outb += reply.encode()
         else:
             print('closing connection to', data.addr)
             sel.unregister(sock)
